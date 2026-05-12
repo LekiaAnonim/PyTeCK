80209b4039e029b1474aa0d20e58a2249ef14c27
Lekia Prosper <lekia.p@northeastern.edu>
2026-05-12 11:55:37 -0400

    Removed the example output artifact from commit

--- examples/h2o2-shocktube/main.py
+++ examples/h2o2-shocktube/main.py
@@ -5,12 +5,10 @@ from __future__ import division
 # Local imports
 from pyteck.eval_model import evaluate_model
 
-if __name__ == '__main__':
-    evaluate_model(model_name='h2o2.cti',
-                   spec_keys_file='spec_keys.yaml',
-                   dataset_file='dataset_file.txt',
-                   data_path='data',
-                   model_path='models',
-                   results_path='results',
-                   skip_validation=True,
-    )
+evaluate_model(model_name='h2o2.cti',
+               spec_keys_file='spec_keys.yaml',
+               dataset_file='dataset_file.txt',
+               data_path='data',
+               model_path='models',
+               results_path='results',
+)
